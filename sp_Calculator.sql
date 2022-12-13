241161016c266294143b7f6b4af170f904c0f42e
vk185201 <119923784+vk185201@users.noreply.github.com>
2022-12-13 16:26:06 +0530

    update with try catch

--- sp_Calculator.sql
+++ sp_Calculator.sql
@@ -3,18 +3,27 @@ create proc sp_Calculator
 @operand_1 int, @operand_2 int,@operation int,@result int output
 As 
 begin
-	if(@operation=1) 
-		set @result=@operand_1+@operand_2
-	else if(@operation=2)
-		set @result=@operand_1-@operand_2
-	else if (@operation=3)
-		set @result=@operand_1*@operand_2
-	else if (@operation=4)
-		set @result=@operand_1/@operand_2
-	else 
-		set @result=@operand_1%@operand_2
+	begin try 
+		if(@operation=1) 
+			set @result=@operand_1+@operand_2
+		else if(@operation=2)
+			set @result=@operand_1-@operand_2
+		else if (@operation=3)
+			set @result=@operand_1*@operand_2
+		else if (@operation=4)
+			set @result=@operand_1/@operand_2
+		else 
+			set @result=@operand_1%@operand_2
+		select @result as Result
+	end try
+	begin catch
+		select ERROR_NUMBER() as ErrorNummber,
+			   ERROR_MESSAGE() as ErrorMessage,
+			   ERROR_STATE() as ErrorState,
+			   ERROR_LINE()as ErrorLine,
+			   ERROR_PROCEDURE() as ErrorProcedure
+	end catch
 end
-select @result as Result
 Go
 
 print '1 for addition'
@@ -22,8 +31,10 @@ print '2 for Subtraction'
 print '3 for Multiplication'
 print '4 for Divison'
 print '5 for Modulus'
-exec sp_Calculator 20,10,1,0
+declare @result int
+
+exec sp_Calculator 20,10,1,@result out
 sp_Calculator 20,10,2,0
 sp_Calculator 20,10,3,0
-sp_Calculator 20,10,4,0
+sp_Calculator 20,0,4,0
 sp_Calculator 20,10,5,0
